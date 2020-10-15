3d53f554642e9b589c67782c1a836c2fb1526b7b
Geoffrey Hil <g.hill@sae.edu>
2020-10-15 12:04:29 +1100

    Initial commit.

---
+++ Assets/TestingInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingInput : MonoBehaviour
+{
+    public GameObject cube;
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = cube.transform.gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Controller activeController = OVRInput.GetActiveController();
+
+
+        float indexTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+        if (indexTrigger != 0.0f)
+        {
+
+            r.material.color = Color.blue;
+        }
+
+
+    }
+}
