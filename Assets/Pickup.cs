41d88700447a7596b77f8f8f39c6ef254acc4b49
Geoffrey Hil <g.hill@sae.edu>
2020-10-15 12:50:38 +1100

    Added color change on hover.

---
+++ Assets/Pickup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Pickup")
+        {
+
+            //set the color of the object to red
+
+            float indexTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+            if (indexTrigger != 0.0f)
+            {
+                SetColor otherColor = other.gameObject.GetComponent<SetColor>();
+                otherColor.SetGreen();
+
+            }
+
+
+            //check if i have pinched OR if the controller button has gone down while i have gone over
+
+            //pickup the object by turning off its rigidbody and then moving it to the position of the hand
+
+        }
+    }
+}
---
+++ Assets/SetColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor : MonoBehaviour
+{
+
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetGreen()
+    {
+        r.material.color = Color.green;
+
+        //build an thread the loops through colour until its green from red.
+
+    }
+
+    public void SetRed()
+    {
+        r.material.color = Color.red;
+
+    }
+}
