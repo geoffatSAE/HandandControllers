f8192cfff05b005c89455fc9b2c92b63aff7d1ee
Geoffrey Hil <g.hill@sae.edu>
2020-11-05 08:56:38 +1100

    Added haptic feedback.

--- Assets/TestingInput.cs
+++ Assets/TestingInput.cs
@@ -23,9 +23,32 @@ public class TestingInput : MonoBehaviour
         if (indexTrigger != 0.0f)
         {
 
-            r.material.color = Color.blue;
+            r.material.color = Color.blue; //
+            OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.LTouch);
+
+            StartCoroutine("WaitAndStopHaptics"); //stop hapic feedback after 0.2 seconds
+        }
+
+        float secindexTrigger = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+        if (secindexTrigger != 0.0f)
+        {
+
+            r.material.color = Color.cyan; //
+            OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+
         }
 
 
+
+
+    }
+
+    IEnumerator WaitAndStopHaptics()
+    {
+        // suspend execution for 1 seconds
+        yield return new WaitForSeconds(0.2f);
+        OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+        OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
+        print("Wait Stop Haptics completed.");
     }
 }
